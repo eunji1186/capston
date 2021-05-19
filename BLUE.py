576d794c8ddcc3c2c74c25bae93db0ca1062e582
eunji1186 <eunji1186@kakao.com>
2021-05-19 18:09:56 +0900

    BLUE code change

--- BLUE.py
+++ BLUE.py
@@ -1,37 +1,44 @@
 import bluetooth
-
+import subprocess
+class BluetoothComm:
+    def __init__(self):
+        self.server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+        port = 1
+        self.server_socket.bind(("",port))
+        self.server_socket.listen(1)
+        self.client_socket,address = self.server_socket.accept()
+        print("Accepted connection from ",address)
+        
+    def read_comm(self):
+        res = self.client_socket.recv(1024)
+        if len(res):
+            return res
+        else:
+            return None
  
+    def send_comm(self, text):
+        self.client_socket.send(text)
+        
+def main():
+    blue_comm = BluetoothComm()
+    buff_msg = b''
+    while True:
+        get = blue_comm.read_comm()
+        blue_comm.send_comm(get)
+        print(get)
 
-server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-
- 
-
-port = 1
-
-server_socket.bind(("",port))
-
-server_socket.listen(1)
-
- 
-
-client_socket,address = server_socket.accept()
-
-print("Accepted connection from ",address)
-
-while 1:
-
- data = client_socket.recv(1024)
-
- print("Received: %s" % data)
-
- if (data == "q"):
-
-  print ("Quit")
-
-  break
-
- 
-
-client_socket.close()
-
-server_socket.close()
+        if get != b'\r':
+            buff_msg += get
+        else:
+            print(buff_msg)
+            p = subprocess.Popen([b'bash',b'-c',buff_msg], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            shell_out = p.communicate()
+            print('shell:',shell_out)
+            blue_comm.send_comm('\n')           
+            if shell_out[0] != b'':
+                blue_comm.send_comm(shell_out[0])
+            else:
+                blue_comm.send_comm(shell_out[1])
+            blue_comm.send_comm('\r')
+            buff_msg = b''
+main()
